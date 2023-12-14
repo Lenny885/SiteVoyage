a8d3340ba86f41859eee9ba16db370537df58cc6
Lenny885 <151829610+Lenny885@users.noreply.github.com>
2023-12-14 11:23:03 +0100

    Update bd_sitevoyage.sql

--- bd_sitevoyage.sql
+++ bd_sitevoyage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 14 déc. 2023 à 09:40
+-- Généré le : jeu. 14 déc. 2023 à 10:21
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -22,6 +22,24 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `bd_sitevoyage` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `bd_sitevoyage`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `avt_aventure`
+--
+
+DROP TABLE IF EXISTS `avt_aventure`;
+CREATE TABLE IF NOT EXISTS `avt_aventure` (
+  `AVT_IdAventure` int(11) NOT NULL AUTO_INCREMENT,
+  `AVT_NomAventure` varchar(38) NOT NULL,
+  `AVT_DescAventure` text NOT NULL,
+  `AVT_NomLocAventure1` varchar(38) DEFAULT NULL,
+  `AVT_DescLocAventure1` text,
+  `AVT_NomLoc Aventure2` varchar(38) DEFAULT NULL,
+  `AVT_DescLocAventure2` text,
+  PRIMARY KEY (`AVT_IdAventure`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -46,15 +64,12 @@ DROP TABLE IF EXISTS `loc_location`;
 CREATE TABLE IF NOT EXISTS `loc_location` (
   `LOC_IdLocation` int(11) NOT NULL AUTO_INCREMENT,
   `LOC_NomLocation` varchar(38) NOT NULL,
-  `CAT_IdCategorie` int(11) NOT NULL,
-  `PAY_IdPays` int(11) NOT NULL,
   `LOC_NoteLocation` int(2) DEFAULT NULL,
   `LOC_DescLocation` text NOT NULL,
   `LOC_AvisPerso` text,
   `LOC_DateLocation` date NOT NULL,
-  PRIMARY KEY (`LOC_IdLocation`),
-  KEY `CAT_IdCategorie` (`CAT_IdCategorie`),
-  KEY `PAY_IdPays` (`PAY_IdPays`)
+  `LOC_ImgLocation` varchar(20) NOT NULL,
+  PRIMARY KEY (`LOC_IdLocation`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -68,19 +83,39 @@ CREATE TABLE IF NOT EXISTS `pay_pays` (
   `PAY_idPays` int(11) NOT NULL AUTO_INCREMENT,
   `PAY_NomPays` varchar(38) NOT NULL,
   `PAY_NotePays` int(2) DEFAULT NULL,
-  PRIMARY KEY (`PAY_idPays`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `LOC_IdLocation` int(11) DEFAULT NULL,
+  `PAY_DescPays` text,
+  `PAY_Avantage` text,
+  `PAY_Inconvenient` text,
+  `CAT_IdCategorie` int(11) DEFAULT NULL,
+  `PAY_ImgPays` int(11) DEFAULT NULL,
+  PRIMARY KEY (`PAY_idPays`),
+  KEY `CAT_IdCategorie` (`CAT_IdCategorie`),
+  KEY `LOC_IdLocation` (`LOC_IdLocation`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `pay_pays`
+--
+
+INSERT INTO `pay_pays` (`PAY_idPays`, `PAY_NomPays`, `PAY_NotePays`, `LOC_IdLocation`, `PAY_DescPays`, `PAY_Avantage`, `PAY_Inconvenient`, `CAT_IdCategorie`, `PAY_ImgPays`) VALUES
+(7, 'FRANCE', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(8, 'ITALIE', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(9, 'ESPAGNE', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(10, 'BRESIL', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(11, 'CANADA', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(12, 'MEXIQUE', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `loc_location`
+-- Contraintes pour la table `pay_pays`
 --
-ALTER TABLE `loc_location`
-  ADD CONSTRAINT `loc_location_ibfk_1` FOREIGN KEY (`CAT_IdCategorie`) REFERENCES `cat_categorie` (`CAT_IdCategorie`),
-  ADD CONSTRAINT `loc_location_ibfk_2` FOREIGN KEY (`PAY_IdPays`) REFERENCES `pay_pays` (`PAY_idPays`);
+ALTER TABLE `pay_pays`
+  ADD CONSTRAINT `pay_pays_ibfk_1` FOREIGN KEY (`CAT_IdCategorie`) REFERENCES `cat_categorie` (`CAT_IdCategorie`),
+  ADD CONSTRAINT `pay_pays_ibfk_2` FOREIGN KEY (`LOC_IdLocation`) REFERENCES `loc_location` (`LOC_IdLocation`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
